df88b30fcfb6a9683838e5371cad49c55b81fb9e
Bartek Hejduk <barhej@st.amu.edu.pl>
2019-06-25 16:10:28 +0200

    Add application

---
+++ GusApplication/Data/Połącz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using GusApplication.GusAPI;
+
+namespace GusApplication.Data
+{
+    class Połącz
+    {
+        private UslugaBIRzewnPublClient klient;
+
+        public UslugaBIRzewnPublClient Klient
+        {
+            get { return this.klient; }
+        }
+
+        public Połącz(UslugaBIRzewnPublClient klient)
+        {
+            this.klient = klient;
+        }
+        public string Zaloguj()
+        {
+            string sid = this.klient.Zaloguj("abcde12345abcde12345");
+            OperationContextScope scope = new OperationContextScope(klient.InnerChannel);
+
+            HttpRequestMessageProperty reqProps = new HttpRequestMessageProperty();
+            reqProps.Headers.Add("sid", sid);
+            OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = reqProps;
+
+            return sid;
+        }
+
+        public void Wylogoj(string adres)
+        {
+            klient.Wyloguj(adres);
+        }
+    }
+}
---
+++ GusApplication/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using GusApplication.GusAPI;
+using GusApplication.Data;
+
+namespace GusApplication
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+public partial class MainWindow : Window
+{
+    Połącz połącz = new Połącz(new UslugaBIRzewnPublClient());
+    string sid;
+    public MainWindow()
+    {
+        InitializeComponent();
+        sid = połącz.Zaloguj();
+    }
+
+    //Przekazanie numeru NIP do obiektu, którego metoda wyszukiwania Gusu odczytuje
+    public ParametryWyszukiwania GetParametry(string nip)
+    {
+        return new ParametryWyszukiwania
+        {
+            Nip = nip
+        };
+    }
+
+    //Pobranie wartości zawartej między znacznikiem podanym jako parametr
+    public string TekstElementuXMl(XmlDocument doc, string element)
+    {
+        return doc.SelectSingleNode("root/dane/" + element).InnerText;
+    }
+
+    //Sprawdzanie czy Firma o podanym numerze NIP istnieje, przz sprawdzenie nazwy pierwszego wewnętrznego znacznika
+    public bool CzyNipIstnieje(XmlDocument doc)
+    {
+        XmlNode pierwszyZnacznik = doc.SelectSingleNode("/root/dane[1]");
+        if (pierwszyZnacznik.FirstChild.Name == "ErrorCode")
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //Przycisk 
+    private void SzukajButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (nipTextBox.Text.Length != 10) //Sprawdzanie czy podany łańcuch ma 10 znaków
+            {
+                MessageBox.Show("Numer NIP musi mieć 10 znaków");
+            }
+            else
+            {
+                ParametryWyszukiwania parametr = GetParametry(nipTextBox.Text);
+                var firma = połącz.Klient.DaneSzukajPodmioty(parametr);
+                XmlDocument firmaXML = new XmlDocument();
+                firmaXML.LoadXml(firma);
+
+                if (CzyNipIstnieje(firmaXML)) //Wypełnianie okna informacjami o firmie
+                {
+                    regonLabel.Content = TekstElementuXMl(firmaXML, "Regon");
+                    nazwaTexTBlock.Text = TekstElementuXMl(firmaXML, "Nazwa");
+                    województwoLabel.Content = TekstElementuXMl(firmaXML, "Wojewodztwo");
+                    adresLabel.Content = TekstElementuXMl(firmaXML, "Miejscowosc") + " " + TekstElementuXMl(firmaXML, "Ulica") + " " + TekstElementuXMl(firmaXML, "NrNieruchomosci");
+                    gminaLabel.Content = TekstElementuXMl(firmaXML, "Gmina");
+                    typLabel.Content = TekstElementuXMl(firmaXML, "Typ");
+                    kodPocztowyLabel.Content = TekstElementuXMl(firmaXML, "KodPocztowy");
+                    powiatLabel.Content = TekstElementuXMl(firmaXML, "Powiat");
+                }
+                else
+                {
+                    MessageBox.Show("Brak firmy o podanym numerze NIP");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        połącz.Wylogoj(sid);
+    }
+}
+
+}
